e2d83161e38f1857a76315de903f7aeae9ae90a9
Vincent Emonet <vincent.emonet@gmail.com>
2021-02-12 19:18:27 +0100

    fix prod tests

--- tests/test_production_api.py
+++ tests/test_production_api.py
@@ -8,7 +8,7 @@ def test_post_trapi():
     """Test Translator ReasonerAPI query POST operation to get predictions"""
     trapi_query = {
         "message": {
-            "n_results": 42,
+            "n_results": 13,
             "query_graph": {
             "edges": {
                 "e01": {
@@ -33,5 +33,5 @@ def test_post_trapi():
                         data=json.dumps(trapi_query), headers=headers).json()
 
     edges = trapi_results['knowledge_graph']['edges'].items()
-    assert len(edges) == 42
-    # assert edges[0]['object'] == 'OMIM:246300'
+    assert len(edges) == 13
+    # assert edges[0]['object'] == 'https://identifiers.org/MONDO:0001158'
