0af1febd6168f62d34b36ecce1440d2fa0feca2e
Vincent Emonet <vincent.emonet@gmail.com>
2021-02-19 15:54:57 +0100

    provide queries for tests in files

--- tests/test_production_api.py
+++ tests/test_production_api.py
@@ -6,32 +6,26 @@ PROD_API_URL = 'https://api.collaboratory.semanticscience.org'
 
 def test_post_trapi():
     """Test Translator ReasonerAPI query POST operation to get predictions"""
-    trapi_query = {
-        "message": {
-            "n_results": 13,
-            "query_graph": {
-            "edges": {
-                "e01": {
-                    "object": "n1",
-                    "predicate": "biolink:treats",
-                    "subject": "n0"
-                }
-            },
-            "nodes": {
-                "n0": {
-                    "category": "biolink:Drug"
-                },
-                "n1": {
-                    "category": "biolink:Disease"
-                }
-            }
-            }
-        }
-    }
     headers = {'Content-type': 'application/json'}
-    trapi_results = requests.post(PROD_API_URL + '/query',
-                        data=json.dumps(trapi_query), headers=headers).json()
+    tests_list = [
+        {'limit': 2, 'class': 'drug'},
+        {'limit': 'no', 'class': 'drug'},
+        {'limit': 2, 'class': 'anydrugdisease'},
+        {'limit': 'no', 'class': 'anydrugdisease'},
+    ]
 
-    edges = trapi_results['knowledge_graph']['edges'].items()
-    assert len(edges) >= 5
-    # assert edges[0]['object'] == 'https://identifiers.org/MONDO:0001158'
+    for trapi_test in tests_list:
+        trapi_filename = 'tests/queries/trapi_' + trapi_test['class'] + '_limit' + str(trapi_test['limit']) + '.json'
+        with open(trapi_filename,'r') as f:
+            trapi_query = f.read()
+        trapi_results = requests.post(PROD_API_URL + '/query',
+                            data=trapi_query, headers=headers).json()
+
+        edges = trapi_results['knowledge_graph']['edges'].items()
+
+        if trapi_test['limit'] == 'no':
+            assert len(edges) >= 5
+        else:
+            assert len(edges) == trapi_test['limit']
+
+        # assert edges[0]['object'] == 'https://identifiers.org/MONDO:0001158'
