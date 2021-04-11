05dc141574f6c5bf5c39e8b9708305f1042fde61
Vincent Emonet <vincent.emonet@gmail.com>
2021-04-11 12:22:12 +0200

    improve tests to iterate over queries files

--- tests/test_production_api.py
+++ tests/test_production_api.py
@@ -1,5 +1,5 @@
 import pytest
-import json
+import os
 import requests
 
 PROD_API_URL = 'https://api.collaboratory.semanticscience.org'
@@ -14,18 +14,19 @@ def test_post_trapi():
         {'limit': 'no', 'class': 'anydrugdisease'},
     ]
 
-    for trapi_test in tests_list:
-        trapi_filename = 'tests/queries/trapi_' + trapi_test['class'] + '_limit' + str(trapi_test['limit']) + '.json'
-        with open(trapi_filename,'r') as f:
+    for trapi_filename in os.listdir('tests/queries'):
+    # for trapi_test in tests_list:
+        # trapi_filename = 'tests/queries/trapi_' + trapi_test['class'] + '_limit' + str(trapi_test['limit']) + '.json'
+        with open('tests/queries/' + trapi_filename,'r') as f:
             trapi_query = f.read()
         trapi_results = requests.post(PROD_API_URL + '/query',
                             data=trapi_query, headers=headers).json()
 
         edges = trapi_results['message']['knowledge_graph']['edges'].items()
 
-        if trapi_test['limit'] == 'no':
-            assert len(edges) >= 2
+        if trapi_filename.endswith('limit1.json'):
+            assert len(edges) == 1
         else:
-            assert len(edges) == trapi_test['limit']
+            assert len(edges) >= 2
 
         # assert edges[0]['object'] == 'https://identifiers.org/MONDO:0001158'
